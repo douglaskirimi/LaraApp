38445d7bd55a55b3e1acf4a3e807b7e5ff857454
~Douglas <douglaskirimi98@gmail.com>
2022-12-20 10:59:29 +0300

    part 2 - first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,413 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Douglas Into Laravel</title>
+
+<link rel="stylesheet" href="/css/app.css">
+
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+
+<link rel="stylesheet" href="dist/css/adminlte.min.css?v=3.2.0"> -->
+<!-- <script nonce="58ad78a8-52f6-4a3c-b7e9-15172b1f8013">(function(w,d){!function(eK,eL,eM,eN){eK.zarazData=eK.zarazData||{};eK.zarazData.executed=[];eK.zaraz={deferred:[],listeners:[]};eK.zaraz.q=[];eK.zaraz._f=function(eO){return function(){var eP=Array.prototype.slice.call(arguments);eK.zaraz.q.push({m:eO,a:eP})}};for(const eQ of["track","set","debug"])eK.zaraz[eQ]=eK.zaraz._f(eQ);eK.zaraz.init=()=>{var eR=eL.getElementsByTagName(eN)[0],eS=eL.createElement(eN),eT=eL.getElementsByTagName("title")[0];eT&&(eK.zarazData.t=eL.getElementsByTagName("title")[0].text);eK.zarazData.x=Math.random();eK.zarazData.w=eK.screen.width;eK.zarazData.h=eK.screen.height;eK.zarazData.j=eK.innerHeight;eK.zarazData.e=eK.innerWidth;eK.zarazData.l=eK.location.href;eK.zarazData.r=eL.referrer;eK.zarazData.k=eK.screen.colorDepth;eK.zarazData.n=eL.characterSet;eK.zarazData.o=(new Date).getTimezoneOffset();if(eK.dataLayer)for(const eX of Object.entries(Object.entries(dataLayer).reduce(((eY,eZ)=>({...eY[1],...eZ[1]})))))zaraz.set(eX[0],eX[1],{scope:"page"});eK.zarazData.q=[];for(;eK.zaraz.q.length;){const e_=eK.zaraz.q.shift();eK.zarazData.q.push(e_)}eS.defer=!0;for(const fa of[localStorage,sessionStorage])Object.keys(fa||{}).filter((fc=>fc.startsWith("_zaraz_"))).forEach((fb=>{try{eK.zarazData["z_"+fb.slice(7)]=JSON.parse(fa.getItem(fb))}catch{eK.zarazData["z_"+fb.slice(7)]=fa.getItem(fb)}}));eS.referrerPolicy="origin";eS.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(eK.zarazData)));eR.parentNode.insertBefore(eS,eR)};["complete","interactive"].includes(eL.readyState)?zaraz.init():eK.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script> -->
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+<li class="nav-item">
+<a class="nav-link" data-widget="navbar-search" href="#" role="button">
+<i class="fas fa-search"></i>
+</a>
+<div class="navbar-search-block">
+<form class="form-inline">
+<div class="input-group input-group-sm">
+<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+<div class="input-group-append">
+<button class="btn btn-navbar" type="submit">
+<i class="fas fa-search"></i>
+</button>
+<button class="btn btn-navbar" type="button" data-widget="navbar-search">
+<i class="fas fa-times"></i>
+</button>
+</div>
+</div>
+</form>
+</div>
+</li>
+</li>
+</ul>
+
+<!-- <ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" data-widget="navbar-search" href="#" role="button">
+<i class="fas fa-search"></i>
+</a>
+<div class="navbar-search-block">
+<form class="form-inline">
+<div class="input-group input-group-sm">
+<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+<div class="input-group-append">
+<button class="btn btn-navbar" type="submit">
+<i class="fas fa-search"></i>
+</button>
+<button class="btn btn-navbar" type="button" data-widget="navbar-search">
+<i class="fas fa-times"></i>
+</button>
+</div>
+</div>
+</form>
+</div>
+</li>
+
+<li class="nav-item dropdown">
+<a class="nav-link" data-toggle="dropdown" href="#">
+<i class="far fa-comments"></i>
+<span class="badge badge-danger navbar-badge">3</span>
+</a>
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+<a href="#" class="dropdown-item">
+
+<div class="media">
+<img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+<div class="media-body">
+<h3 class="dropdown-item-title">
+Brad Diesel
+<span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+</h3>
+<p class="text-sm">Call me whenever you can...</p>
+<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+</div>
+</div>
+
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+
+<div class="media">
+<img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+<div class="media-body">
+<h3 class="dropdown-item-title">
+John Pierce
+<span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+</h3>
+<p class="text-sm">I got your message bro</p>
+<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+</div>
+</div>
+
+</a>
+<div class="dropdown-divider"></div>
+ <a href="#" class="dropdown-item">
+
+<div class="media">
+<img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+<div class="media-body">
+<h3 class="dropdown-item-title">
+Nora Silvester
+<span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+</h3>
+<p class="text-sm">The subject goes here</p>
+<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+</div>
+</div>
+
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+</div>
+</li>
+
+<li class="nav-item dropdown">
+<a class="nav-link" data-toggle="dropdown" href="#">
+<i class="far fa-bell"></i>
+<span class="badge badge-warning navbar-badge">15</span>
+</a>
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+<span class="dropdown-header">15 Notifications</span>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+<i class="fas fa-envelope mr-2"></i> 4 new messages
+<span class="float-right text-muted text-sm">3 mins</span>
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+<i class="fas fa-users mr-2"></i> 8 friend requests
+<span class="float-right text-muted text-sm">12 hours</span>
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item">
+<i class="fas fa-file mr-2"></i> 3 new reports
+<span class="float-right text-muted text-sm">2 days</span>
+</a>
+<div class="dropdown-divider"></div>
+<a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+</div>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-widget="fullscreen" href="#" role="button">
+<i class="fas fa-expand-arrows-alt"></i>
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+<i class="fas fa-th-large"></i>
+</a>
+</li>
+</ul> -->
+</nav>
+
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+<a href="index3.html" class="brand-link">
+<img src="/img/9207916.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+<span class="brand-text font-weight-light">Kinyozi POS</span>
+</a>
+
+<div class="sidebar">
+
+<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+<div class="image">
+<img src="/img/9226677.png" class="img-circle elevation-2" alt="User Image">
+</div>
+<div class="info">
+<a href="#" class="d-block">Douglas Kirimi</a>
+</div>
+</div>
+
+<div class="form-inline">
+<div class="input-group" data-widget="sidebar-search">
+<input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+<div class="input-group-append">
+<button class="btn btn-sidebar">
+<i class="fas fa-search fa-fw"></i>
+</button>
+</div>
+</div>
+</div>
+
+<nav class="mt-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+<li class="nav-item menu-closed">
+<a href="#" class="nav-link active">
+<i class="nav-icon fas fa-tachometer-alt"></i>
+<p>
+Dashboard
+<i class="right fas fa-angle-left"></i>
+</p>
+</a>
+<ul class="nav nav-treeview">
+<li class="nav-item">
+<a href="#" class="nav-link active">
+<i class="far fa-circle nav-icon"></i>
+<p>Active Page</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>Inactive Page</p>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="nav-item menu-closed">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-th"></i>
+<p>
+Service Category
+<i class="right fas fa-angle-down"></i>
+</p>
+</a>
+<ul class="nav nav-treeview">
+<li class="nav-item">
+<a href="#" class="nav-link active">
+<i class="far fa-circle nav-icon"></i>
+<p>Add category</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>View categories</p>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="nav-item menu-closed">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-th"></i>
+<p>
+Employees
+<i class="right fas fa-angle-down"></i>
+</p>
+</a>
+<ul class="nav nav-treeview">
+<li class="nav-item">
+<a href="#" class="nav-link active">
+<i class="far fa-circle nav-icon"></i>
+<p>Add employee</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>View employees</p>
+</a>
+</li>
+</ul>
+</li>
+
+<li class="nav-item menu-closed">
+<a href="#" class="nav-link">
+<i class="nav-icon fas fa-th"></i>
+<p>
+Services
+<i class="right fas fa-angle-down"></i>
+</p>
+</a>
+<ul class="nav nav-treeview">
+<li class="nav-item">
+<a href="#" class="nav-link active">
+<i class="far fa-circle nav-icon"></i>
+<p>Add service</p>
+</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">
+<i class="far fa-circle nav-icon"></i>
+<p>View all</p>
+</a>
+</li>
+</ul>
+</li>
+
+</ul>
+</nav>
+
+</div>
+
+</aside>
+
+<div class="content-wrapper">
+
+<div class="content-header">
+<div class="container-fluid">
+<div class="row mb-2">
+<div class="col-sm-6">
+<h1 class="m-0">Starter Page</h1>
+</div>
+<div class="col-sm-6">
+<ol class="breadcrumb float-sm-right">
+<li class="breadcrumb-item"><a href="#">Home</a></li>
+<li class="breadcrumb-item active">Starter Page</li>
+</ol>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="content">
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-6">
+<div class="card">
+<div class="card-body">
+<h5 class="card-title">Card title</h5>
+<p class="card-text">
+Some quick example text to build on the card title and make up the bulk of the card's
+content.
+</p>
+<a href="#" class="card-link">Card link</a>
+<a href="#" class="card-link">Another link</a>
+</div>
+</div>
+<div class="card card-primary card-outline">
+<div class="card-body">
+<h5 class="card-title">Card title</h5>
+<p class="card-text">
+Some quick example text to build on the card title and make up the bulk of the card's
+content.
+</p>
+<a href="#" class="card-link">Card link</a>
+<a href="#" class="card-link">Another link</a>
+</div>
+</div>
+</div>
+
+<div class="col-lg-6">
+<div class="card">
+<div class="card-header">
+<h5 class="m-0">Featured</h5>
+</div>
+<div class="card-body">
+<h6 class="card-title">Special title treatment</h6>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+<div class="card card-primary card-outline">
+<div class="card-header">
+<h5 class="m-0">Featured</h5>
+</div>
+<div class="card-body">
+<h6 class="card-title">Special title treatment</h6>
+<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div>
+ 
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+<aside class="control-sidebar control-sidebar-dark">
+
+<div class="p-3">
+<h5>Title</h5>
+<p>Sidebar content</p>
+</div>
+</aside>
+
+
+<footer class="main-footer">
+
+<div class="float-right d-none d-sm-inline">
+Home of Best Cuts 
+</div>
+
+<strong>Copyright &copy; 2022 <a href="#">Kinyozi-POS</a>.</strong> All rights reserved.
+</footer>
+</div>
+
+
+<script src="/js/app.js"></script>
+
+<!-- 
+
+<script src="plugins/jquery/jquery.min.js"></script>
+
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="dist/js/adminlte.min.js?v=3.2.0"></script> -->
+</body>
+</html>
